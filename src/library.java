d379c967e4e5f53318ff79e015fcf1fa5aea9d86
israel123 <id@studiodvd.co.il>
2016-01-06 11:35:09 +0200

    addd

--- src/library.java
+++ src/library.java
@@ -6,12 +6,12 @@ public class library {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("pls insert number");
-		System.out.println("1 to update book");
-		System.out.println("2 to delete book");
-		System.out.println("3 to find book");
-		System.out.println("4 to print to all books by type");
-		System.out.println("5 to statistics book");
-		System.out.println("6 to insert book");
+		System.out.println("1 to insert book");
+		System.out.println("2 to update book");
+		System.out.println("3 to delete book");
+		System.out.println("4 to find book");
+		System.out.println("5 to print to all books by type");
+		System.out.println("6 to statistics book");
 		System.out.println("7 to insert book");
 		
 		Scanner sc = new Scanner(System.in) ;
